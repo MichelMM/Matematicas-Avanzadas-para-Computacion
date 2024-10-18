1fb517d393d68f03c32a70006f85e935eb63853e
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2024-10-18 16:15:59 -0600

    Update main.py
    Add relevant commentary to each module to facilitate understanding of every function

--- Primera Parte Clasificador Bayesiano Ingenuo/main.py
+++ Primera Parte Clasificador Bayesiano Ingenuo/main.py
@@ -1,7 +1,9 @@
 import re
 import pandas as pd
+#Size of the hash list
 maxsize = 5000
 
+#Node class for linked list in hash list implementation, contains string and points to next node
 class WordNode:
     def __init__(self,word:str)->None:
         self.word = word
@@ -13,24 +15,28 @@ class WordNode:
     def __repr__(self):
         return self.__str__()
 
+#Get value for hashing, uses lowercase letters and extra letters are letters with punctuation that only are used on our 3 used languages, other letters added with different letters return a -1
 def getValue(c):
-    accentLetters = ['ñ','é','à', 'è', 'ù', 'â', 'ê', 'î', 'ô', 'û', 'ë', 'ï', 'ü', 'ÿ', 'ç', 'œ','æ']
+    extraLetters = ['ñ','é','à', 'è', 'ù', 'â', 'ê', 'î', 'ô', 'û', 'ë', 'ï', 'ü', 'ÿ', 'ç', 'œ','æ']
     if ord(c) > 122:
         try :
-            valid = accentLetters.index(c)
-            return 26 + accentLetters.index(c)
+            valid = extraLetters.index(c)
+            return 26 + extraLetters.index(c)
         except ValueError :
             return -1
     return ord(c)-ord('a')
 
-
-
+#Get hash value for a word, multiplies by the size of our letter vocabulary and does a sum with the iteration of the current letter, 
+#use mod operation to keep hash value in range of our hash list size
 def hashCode(word):
     h = getValue(word[0])
     for i in range(1,len(word)):
         h = (h*43 + getValue(word[i]))%maxsize
     return h
 
+#Creates a hash list of maxSize (list of linked list) storing based on hashCode, if a value alredy exists in the current value, 
+#it checks for any string equal tu the one being stored, if it's not the case, it gets stored at the end of the linked list
+#If a hash value returns a negative value, means the searched word would not be stored on our hash list since it would not be a language specific word
 def createHashList(words:list[str]):
     hashList = [None]*maxsize
     for c in words:
@@ -51,7 +57,7 @@ def createHashList(words:list[str]):
             if repeated == 0:
                 curr.next = WordNode(c)
     return hashList
-
+#Get a count of our sample size by going in every element on the list that contains an element and iterating throught the inked list on it
 def sizeHashList(hashList: list[WordNode]):
     count = 0
     for i in range(maxsize):
@@ -62,6 +68,7 @@ def sizeHashList(hashList: list[WordNode]):
                 curr = curr.next
     return count
 
+#Search for a word to be contained in the hash list, if it exists, return True, if not False
 def hashSearch(hashList: list[WordNode], word):
     hash = hashCode(word)
     if hashList[hash]:
@@ -73,7 +80,7 @@ def hashSearch(hashList: list[WordNode], word):
     return False
 
 
-
+#Remove any punctuation and numbers from a text using regex
 def remove_noise(text):
     reggex_punctuation = '([^\s\w])'
     reggex_numbers = '\d'
@@ -81,25 +88,27 @@ def remove_noise(text):
     x = re.sub(reggex_numbers,"",x)
     return x
 
+#calls for remove noise, lowercase all words and finally splits it into a list
 def text_to_list(text):
     text = remove_noise(text)
     words = text.casefold().split()
     return words
 
 
-#Bayes con suavizado de laplace, regresar el resultado mayor
+#Naive bayesian classifier with laplace smoothing, returns the posterior probability with max value
 def naive_Bayesian_classifier(text,dictionary):
-    
+    #text to words
     words = text_to_list(text)
-    
-    relevantData = {}
-    size = "size"
-    class_prior = "class prior"
-    laplace_smoothing = "laplace smoothing"
-    posterior_probability = "posterior probability"
 
-    N = 0
-    marginal_probability = 0
+    #Name any relevant data that will be used in the classifier
+    relevantData = {}
+    size = "size"#Size of the set (hash list)
+    class_prior = "class prior" #Probability of current set (hash list)
+    laplace_smoothing = "laplace smoothing" #likelihood aplied with laplace smoothing
+    posterior_probability = "posterior probability" # our search value to know wich of our sets is the more likely to be correct
+    
+    N = 0 #total size of our sets combined
+    marginal_probability = 0 #total probability of observing a given Bag-of-Words vector across all classes
 
     #set new values to use as relevant data and set size in it
     for key in dictionary:
@@ -110,18 +119,20 @@ def naive_Bayesian_classifier(text,dictionary):
             posterior_probability:0
         }
         N += relevantData[key][size]
-        
+    
+    #set class prior to final values
     for key in relevantData:
         relevantData[key][class_prior] = relevantData[key][size] / N
 
     
-    #Laplace Smoothing
+    #Laplace Smoothing verifying if word is contained in our set (hash list) for every word on the text
     for word in words:
         for key in relevantData:
             exists = 0
             if hashSearch(dictionary[key],word):
                 exists +=1
             relevantData[key][laplace_smoothing] *= (exists + 1)/(N+relevantData[key][size])
+    
     #Marginal Probability
     for key in relevantData:
         marginal_probability += relevantData[key][laplace_smoothing]*relevantData[key][class_prior]
@@ -133,6 +144,7 @@ def naive_Bayesian_classifier(text,dictionary):
     
     result = 0
     language = ""
+    #Verify wich language has more probability to be the correct one
     for key in relevantData:
         if result < relevantData[key][posterior_probability]:
             result = relevantData[key][posterior_probability]
@@ -154,8 +166,10 @@ dictionary = {
     "Spanish":[],
 }
 
+#extraxt values from csv file
 df = pd.read_csv(r'./data/Language Detection.csv')
 
+#Iterate throught every row on the csv and storing relevant ones (ones wich are english, spanish or french) on a respective list for its language
 for i, row in df.iterrows():
     text = row["Text"]
     language = row["Language"]
@@ -165,7 +179,7 @@ for i, row in df.iterrows():
             for e in words:
                 dictionary[key].append(e)
     
-
+#Convert list to hash list
 for key in dictionary:
     dictionary[key] = createHashList(dictionary[key])
 
